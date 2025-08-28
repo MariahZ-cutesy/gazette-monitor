09fc9c84042e0abbdacabfa0fc85fac111a0c09d
MariahZ-cutesy <mariahzaidi.work@gmail.com>
2025-08-28 11:14:51 +0800

    Update gazette_monitor.py

--- gazette_monitor.py
+++ gazette_monitor.py
@@ -5,7 +5,7 @@ st.title("📅 Gazette Issue Comparison")
 st.markdown("Compare this week's Gazette issue with last week's:")
 
 st.image(
-    ["gazette_images/2025-08-21.png", "gazette_images/2025-08-28.png"],
+    ["gazette_snapshots/2025-08-21.png", "gazette_snapshots/2025-08-28.png"],
     caption=["Last Thursday (2025-08-21)", "Today (2025-08-28)"],
     width=600
 )
